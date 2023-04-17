e1c8189412e3e241caa9c26428cbd5167abab247
Alfred <somethingfly@somethingfly.com>
2023-04-16 20:57:01 -0700

    commit tree 383f12712a6f05bb5c7f12af83641f79293b96a3

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,43 @@
-import React from "react";
+// import React from "react";
+import React, { useEffect, useState } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
+// none of this is persisted in a database or connecting to payment system
+// assume this is acceptable at this stage in development
+
 const API = "http://localhost:3001/sushis";
 
+function App() {
+  const [sushis, setSushis] = useState([]);
+  const [plates, setPlates] = useState([]);
+  const [money, setMoney] = useState(100);
+
+  useEffect(() => {
+    fetch(API)
+      .then((r) => r.json())
+      .then((sushis) => setSushis(sushis));
+  }, []);
+
+  function onPlate(sushi) {
+    if ((money - sushi.price) >= 0) {
+      const newMoney = money - sushi.price;
+      setMoney(newMoney);
+      const newPlates = plates;
+      newPlates.push(sushi);
+      setPlates(newPlates);
+    };
+  }
+
+  return (
+    <div className="app">
+      <SushiContainer sushis={sushis} onPlate={onPlate} plates={plates}/>
+      <Table plates={plates} money={money}/>
+  </div>
+  );
+}
+
+/*
 function App() {
   return (
     <div className="app">
@@ -12,5 +46,6 @@ function App() {
     </div>
   );
 }
+*/
 
 export default App;
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import Sushi from "./Sushi"
 import MoreButton from "./MoreButton";
 
-function SushiContainer(props) {
+function SushiContainer({ sushis, onPlate, plates }) {
+  const [show, setShow] = useState(1);
+
+  function onShow() {
+    const newShow = show + 4;
+    setShow(newShow); 
+  }
+
+  const sushisToDisplay = sushis.filter((sushi) => ((sushi.id >= show) && (sushi.id < (show + 4))));
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushisToDisplay.map((sushi) => (
+        <Sushi
+          key={sushi.id}
+          sushi={sushi}
+          onPlate={onPlate}
+          plates={plates}
+         />
+      ))};
+      <MoreButton onShow={onShow} />
     </div>
   );
 }
